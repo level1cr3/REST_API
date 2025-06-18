f109b52720085cb4a192f0dc26324512caf666ce
Yogesh Singh Mehra <yogeshwebdev1@gmail.com>
2025-06-18 12:32:51 +0530

    Add : sdk project and it's consumer

---
+++ Movies.Sdk/ApiEndpoints.cs
@@ -0,0 +1,36 @@
+namespace Movies.Sdk;
+
+public static class ApiEndpoints
+{
+    private const string
+        ApiBase = "/api";
+
+    public static class V1
+    {
+        private const string VersionBase = $"{ApiBase}/v1";
+
+        public static class Movies
+        {
+            private const string Base = $"{VersionBase}/Movies";
+
+            public const string Create = Base;
+
+            public const string
+                Get = $"{Base}/{{idOrSlug}}"; 
+
+            public const string GetAll = Base;
+            public const string Update = $"{Base}/{{id:guid}}";
+            public const string Delete = $"{Base}/{{id:guid}}";
+
+            public const string Rate = $"{Base}/{{id:guid}}/ratings";
+            public const string DeleteRating = $"{Base}/{{id:guid}}/ratings";
+        }
+
+        public static class Ratings
+        {
+            private const string Base = $"{VersionBase}/ratings";
+
+            public const string GetUserRatings = $"{Base}/me";
+        }
+    }
+}
