4cc4a9460abd63737c24db5a9543f2958b7b609a
Yogesh Singh Mehra <yogeshwebdev1@gmail.com>
2025-06-18 14:42:39 +0530

    Add : Get all movies in the consumer

--- Movies.Sdk.Consumer/Program.cs
+++ Movies.Sdk.Consumer/Program.cs
@@ -1,14 +1,29 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
+using Movies.Contracts.Requests.V1;
 using Movies.Sdk;
 using Refit;
 
 var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
 
-var movie = moviesApi.GetMovieAsync("the-clean-greek-2024");
+// var movie = moviesApi.GetMovieAsync("the-clean-greek-2024");
 
-Console.WriteLine(JsonSerializer.Serialize(movie));
+// Console.WriteLine(JsonSerializer.Serialize(movie));
 
+var getAllMoviesRequest = new GetAllMoviesRequest
+{
+    SortBy = null,
+    Title = null,
+    YearOfRelease = null,
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(getAllMoviesRequest);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
 
 /*
  return object is returned only on 2XX status codes response. any bad path will throw apiException and this exception needs to parsed to get appropriate erros.
--- Movies.Sdk/IMoviesApi.cs
+++ Movies.Sdk/IMoviesApi.cs
@@ -1,4 +1,5 @@
-﻿using Movies.Contracts.Responses.V1;
+﻿using Movies.Contracts.Requests.V1;
+using Movies.Contracts.Responses.V1;
 using Refit;
 
 namespace Movies.Sdk;
@@ -8,4 +9,7 @@ public interface IMoviesApi
 {
     [Get(ApiEndpoints.V1.Movies.Get)]
     Task<MovieResponse> GetMovieAsync(string idOrSlug);
+
+    [Get(ApiEndpoints.V1.Movies.GetAll)]
+    Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request);
 }
